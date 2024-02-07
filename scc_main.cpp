921d9b9c0b84ae5b135649a1af431b2f1349f054
logs <logs@gmai.com>
2024-02-07 06:24:58 +0530

    Passing deleted edges to insertion

--- scc_main.cpp
+++ scc_main.cpp
@@ -43,16 +43,15 @@
 //@HEADER
 */
 
-
 using namespace std;
 
+#include <assert.h>
 #include <cstdlib>
 #include <fstream>
-#include <sys/time.h>
-#include <string.h>
-#include <assert.h>
-#include <vector>
 #include <omp.h>
+#include <string.h>
+#include <sys/time.h>
+#include <vector>
 
 #define VERBOSE 0
 #define DEBUG 0
@@ -66,55 +65,51 @@ using namespace std;
 #define ALPHA 15.0
 #define BETA 25
 
-
-double timer()
-{
+double timer() {
   struct timeval tp;
   gettimeofday(&tp, NULL);
-  return ((double) (tp.tv_sec) + 1e-6 * tp.tv_usec);
+  return ((double)(tp.tv_sec) + 1e-6 * tp.tv_usec);
 }
 
 typedef struct graph {
   int n;
   unsigned m;
-  int* out_array;
-  int* in_array;
-  unsigned* out_degree_list;
-  unsigned* in_degree_list;
+  int *out_array;
+  int *in_array;
+  unsigned *out_degree_list;
+  unsigned *in_degree_list;
   int scc_count; // no.of scc's
-  int* scc_map;
-  int* rep_nodes; // array keeping count in 
-  int* count_in_sccs; //
+  int *scc_map;
+  int *rep_nodes;     // array keeping count in
+  int *count_in_sccs; //
 } graph;
-#define out_degree(g, n) (g.out_degree_list[n+1] - g.out_degree_list[n])
-#define in_degree(g, n) (g.in_degree_list[n+1] - g.in_degree_list[n])
+#define out_degree(g, n) (g.out_degree_list[n + 1] - g.out_degree_list[n])
+#define in_degree(g, n) (g.in_degree_list[n + 1] - g.in_degree_list[n])
 #define out_vertices(g, n) &g.out_array[g.out_degree_list[n]]
 #define in_vertices(g, n) &g.in_array[g.in_degree_list[n]]
 
-#include "scc_trim.cpp"
-#include "scc_fwbw.cpp"
 #include "scc_color.cpp"
-#include "scc_serial.cpp"
-#include "scc_verify.cpp"
 #include "scc_dynamic_parts.cpp"
+#include "scc_fwbw.cpp"
+#include "scc_serial.cpp"
+#include "scc_trim.cpp"
+#include "scc_verify.cpp"
 
-int common_read_edge(char* filename,
-  int& n, unsigned& m,
-  int*& srcs, int*& dsts)
-{
-//  ifstream infile;
-//  string line;
-//  infile.open(filename);
-  printf("reading %s\n",filename);
-  FILE *file = fopen(filename,"r"); // opening the file
+int common_read_edge(char *filename, int &n, unsigned &m, int *&srcs,
+                     int *&dsts) {
+  //  ifstream infile;
+  //  string line;
+  //  infile.open(filename);
+  printf("reading %s\n", filename);
+  FILE *file = fopen(filename, "r"); // opening the file
   if (fscanf(file, "%d %d", &n, &m) != 2) {
-        fprintf(stderr, "Invalid format for n and m in the first line\n");
-        fclose(file);
-        return 1;
-    }
+    fprintf(stderr, "Invalid format for n and m in the first line\n");
+    fclose(file);
+    return 1;
+  }
 
 #if DEBUG
-  printf("vertices:%d, edges:%d\n",n,m);
+  printf("vertices:%d, edges:%d\n", n, m);
 #endif
 
   int src, dest;
@@ -126,11 +121,11 @@ int common_read_edge(char* filename,
 #if DEBUG
 //        printf("Source: %d, Destination: %d\n", src, dest);
 #endif
-	srcs[counter] = src;
-	dsts[counter] = dest;
-	++counter;
-    }
-  
+    srcs[counter] = src;
+    dsts[counter] = dest;
+    ++counter;
+  }
+
   fclose(file);
   return 0;
 }
@@ -171,33 +166,31 @@ int common_read_edge(char* filename,
   infile.close();
 }*/
 
-void create_csr(int n, unsigned m, 
-  int* srcs, int* dsts,
-  int*& out_array, int*& in_array,
-  unsigned*& out_degree_list, unsigned*& in_degree_list,
-  int& max_deg_vert, double& avg_degree)
-{
+void create_csr(int n, unsigned m, int *srcs, int *dsts, int *&out_array,
+                int *&in_array, unsigned *&out_degree_list,
+                unsigned *&in_degree_list, int &max_deg_vert,
+                double &avg_degree) {
   out_array = new int[m];
   in_array = new int[m];
-  out_degree_list = new unsigned[n+1];
-  in_degree_list = new unsigned[n+1];
+  out_degree_list = new unsigned[n + 1];
+  in_degree_list = new unsigned[n + 1];
 
   for (unsigned i = 0; i < m; ++i)
     out_array[i] = 0;
   for (unsigned i = 0; i < m; ++i)
-    in_array[i] = 0;  
-  for (int i = 0; i < n+1; ++i)
+    in_array[i] = 0;
+  for (int i = 0; i < n + 1; ++i)
     out_degree_list[i] = 0;
-  for (int i = 0; i < n+1; ++i)
+  for (int i = 0; i < n + 1; ++i)
     in_degree_list[i] = 0;
 
-  unsigned* temp_counts = new unsigned[n];
+  unsigned *temp_counts = new unsigned[n];
   for (int i = 0; i < n; ++i)
     temp_counts[i] = 0;
   for (unsigned i = 0; i < m; ++i)
     ++temp_counts[srcs[i]];
   for (int i = 0; i < n; ++i)
-    out_degree_list[i+1] = out_degree_list[i] + temp_counts[i];
+    out_degree_list[i + 1] = out_degree_list[i] + temp_counts[i];
   copy(out_degree_list, out_degree_list + n, temp_counts);
   for (unsigned i = 0; i < m; ++i)
     out_array[temp_counts[srcs[i]]++] = dsts[i];
@@ -207,22 +200,20 @@ void create_csr(int n, unsigned m,
   for (unsigned i = 0; i < m; ++i)
     ++temp_counts[dsts[i]];
   for (int i = 0; i < n; ++i)
-    in_degree_list[i+1] = in_degree_list[i] + temp_counts[i];
+    in_degree_list[i + 1] = in_degree_list[i] + temp_counts[i];
   copy(in_degree_list, in_degree_list + n, temp_counts);
   for (unsigned i = 0; i < m; ++i)
     in_array[temp_counts[dsts[i]]++] = srcs[i];
-  delete [] temp_counts;
+  delete[] temp_counts;
 
   avg_degree = 0.0;
   double max_degree = 0.0;
-  for (int i = 0; i < n; ++i)
-  {
-    unsigned out_degree = out_degree_list[i+1] - out_degree_list[i];
-    unsigned in_degree = in_degree_list[i+1] - in_degree_list[i];
+  for (int i = 0; i < n; ++i) {
+    unsigned out_degree = out_degree_list[i + 1] - out_degree_list[i];
+    unsigned in_degree = in_degree_list[i + 1] - in_degree_list[i];
     double degree = (double)out_degree * (double)in_degree;
     avg_degree += (double)out_degree;
-    if (degree > max_degree)
-    {
+    if (degree > max_degree) {
       max_deg_vert = i;
       max_degree = degree;
     }
@@ -230,17 +221,16 @@ void create_csr(int n, unsigned m,
   avg_degree /= (double)n;
 
 #if DEBUG
-  int max_out = out_degree_list[max_deg_vert+1] - out_degree_list[max_deg_vert];
-  int max_in = in_degree_list[max_deg_vert+1] - in_degree_list[max_deg_vert];
+  int max_out =
+      out_degree_list[max_deg_vert + 1] - out_degree_list[max_deg_vert];
+  int max_in = in_degree_list[max_deg_vert + 1] - in_degree_list[max_deg_vert];
   int max_tot = max_out > max_in ? max_out : max_in;
-  printf("max deg vert: %d (%d), avg_degree %9.2lf\n", max_deg_vert, max_tot, avg_degree);
+  printf("max deg vert: %d (%d), avg_degree %9.2lf\n", max_deg_vert, max_tot,
+         avg_degree);
 #endif
 }
 
-
-
-void output_scc(graph& g, int* scc_maps, char* output_file)
-{
+void output_scc(graph &g, int *scc_maps, char *output_file) {
   std::ofstream outfile;
   outfile.open(output_file);
 
@@ -250,15 +240,13 @@ void output_scc(graph& g, int* scc_maps, char* output_file)
   outfile.close();
 }
 
-void print_usage(char** argv)
-{
+void print_usage(char **argv) {
   printf("Usage: %s [graph] [optional: output file]\n", argv[0]);
   exit(0);
 }
 
-void run_scc(graph& g, int*& scc_maps,
-  int max_deg_vert, double avg_degree, int vert_cutoff)
-{
+void run_scc(graph &g, int *&scc_maps, int max_deg_vert, double avg_degree,
+             int vert_cutoff) {
 
   int num_trim = 0;
   int num_fwbw = 0;
@@ -272,44 +260,39 @@ void run_scc(graph& g, int*& scc_maps,
 #endif
 
   scc_maps = new int[g.n];
-  bool* valid = new bool[g.n];
-  int* valid_verts = new int[g.n];
+  bool *valid = new bool[g.n];
+  int *valid_verts = new int[g.n];
   int num_valid = g.n;
 
 #pragma omp parallel
-{
+  {
 #pragma omp for nowait
-  for (int i = 0; i < g.n; ++i) valid[i] = true;
+    for (int i = 0; i < g.n; ++i)
+      valid[i] = true;
 #pragma omp for nowait
-  for (int i = 0; i < g.n; ++i) scc_maps[i] = -1;
+    for (int i = 0; i < g.n; ++i)
+      scc_maps[i] = -1;
 #pragma omp for nowait
-  for (int i = 0; i < g.n; ++i) valid_verts[i] = i;
-}
+    for (int i = 0; i < g.n; ++i)
+      valid_verts[i] = i;
+  }
 
-
-if (TRIM_LEVEL == 0)
-{
-/*  num_trim = scc_trim_none(g, valid,
-    valid_verts, num_valid,
-    scc_maps);*/
-}
-else if (TRIM_LEVEL == 1)
-{
-  num_trim = scc_trim(g, valid, 
-    valid_verts, num_valid, 
-    scc_maps);
-}
-else if (TRIM_LEVEL == 2)
-{
-  num_trim = scc_trim_complete(g, valid,
-    valid_verts, num_valid,
-    scc_maps);
-  num_valid = 0;
-  for (int i = 0; i < g.n; ++i) if (valid[i]) valid_verts[num_valid++] = i;
-}
+  if (TRIM_LEVEL == 0) {
+    /*  num_trim = scc_trim_none(g, valid,
+        valid_verts, num_valid,
+        scc_maps);*/
+  } else if (TRIM_LEVEL == 1) {
+    num_trim = scc_trim(g, valid, valid_verts, num_valid, scc_maps);
+  } else if (TRIM_LEVEL == 2) {
+    num_trim = scc_trim_complete(g, valid, valid_verts, num_valid, scc_maps);
+    num_valid = 0;
+    for (int i = 0; i < g.n; ++i)
+      if (valid[i])
+        valid_verts[num_valid++] = i;
+  }
 
 #if VERIFY
-  scc_verify(g, scc_maps);  
+  scc_verify(g, scc_maps);
 #endif
 
 #if VERBOSE
@@ -319,13 +302,11 @@ else if (TRIM_LEVEL == 2)
   elt = timer();
 #endif
 
-  num_fwbw = scc_fwbw(g, valid, 
-    valid_verts, num_valid, 
-    max_deg_vert, avg_degree,
-    scc_maps);
+  num_fwbw = scc_fwbw(g, valid, valid_verts, num_valid, max_deg_vert,
+                      avg_degree, scc_maps);
 
 #if VERIFY
-  scc_verify(g, scc_maps);  
+  scc_verify(g, scc_maps);
 #endif
 
 #if VERBOSE
@@ -334,14 +315,12 @@ else if (TRIM_LEVEL == 2)
   printf("Performing Coloring step ... \n");
   elt = timer();
 #endif
-  
-  num_color = scc_color(g, valid,
-    valid_verts, num_valid, 
-    vert_cutoff,
-    scc_maps);
+
+  num_color =
+      scc_color(g, valid, valid_verts, num_valid, vert_cutoff, scc_maps);
 
 #if VERIFY
-  scc_verify(g, scc_maps);  
+  scc_verify(g, scc_maps);
 #endif
 
 #if VERBOSE
@@ -350,10 +329,8 @@ else if (TRIM_LEVEL == 2)
   printf("Performing Serial step ... \n");
   elt = timer();
 #endif
- 
-  tarjan t(g, valid,
-    valid_verts, num_valid, 
-    scc_maps);
+
+  tarjan t(g, valid, valid_verts, num_valid, scc_maps);
   num_serial = t.run_valid();
 
 #if VERBOSE
@@ -363,63 +340,63 @@ else if (TRIM_LEVEL == 2)
   printf("TOTAL: %9.6lf\n", start_time);
 #endif
 
- delete [] valid;
- delete [] valid_verts;
+  delete[] valid;
+  delete[] valid_verts;
 }
 
-void print_scc(graph& g){
-        for(int i=0; i<g.n; i++){
-                printf("SCC of vertice %d is %d\n",i,g.scc_map[i]);
-        }
+void print_scc(graph &g) {
+  for (int i = 0; i < g.n; i++) {
+    printf("SCC of vertice %d is %d\n", i, g.scc_map[i]);
+  }
 }
 
-void update_g_with_scc(graph& g){
-	int scc_count=0;
-	int* scc_map = g.scc_map;
-	int* rep;
-	int* counts;
-	int curr_index = 0;
-	unordered_map<int, int> temp_indexes;
-	for(int i=0; i<g.n; i++){
-		if(temp_indexes.find(scc_map[i])==temp_indexes.end()){
-			temp_indexes[scc_map[i]] = curr_index++;
-			scc_count++;
-		}
-	}
-	// making it temporarily g.n, will see if realloncation can be done starting with smaller arrays
-//	rep = new int[scc_count];
-//	counts = new int[scc_count];
-	rep = new int[g.n];
-	counts = new int[g.n];
+void update_g_with_scc(graph &g) {
+  int scc_count = 0;
+  int *scc_map = g.scc_map;
+  int *rep;
+  int *counts;
+  int curr_index = 0;
+  unordered_map<int, int> temp_indexes;
+  for (int i = 0; i < g.n; i++) {
+    if (temp_indexes.find(scc_map[i]) == temp_indexes.end()) {
+      temp_indexes[scc_map[i]] = curr_index++;
+      scc_count++;
+    }
+  }
+  // making it temporarily g.n, will see if realloncation can be done starting
+  // with smaller arrays
+  //	rep = new int[scc_count];
+  //	counts = new int[scc_count];
+  rep = new int[g.n];
+  counts = new int[g.n];
 
-	for(int i=0; i<g.n; i++){
-		counts[i] = 0;
-	}
-	for(int i=0; i<g.n; i++){
-		rep[temp_indexes[scc_map[i]]] = scc_map[i];
-		counts[temp_indexes[scc_map[i]]]++;
-	}
-	g.scc_count = scc_count;
-	g.rep_nodes = rep;
-	g.count_in_sccs = counts;
+  for (int i = 0; i < g.n; i++) {
+    counts[i] = 0;
+  }
+  for (int i = 0; i < g.n; i++) {
+    rep[temp_indexes[scc_map[i]]] = scc_map[i];
+    counts[temp_indexes[scc_map[i]]]++;
+  }
+  g.scc_count = scc_count;
+  g.rep_nodes = rep;
+  g.count_in_sccs = counts;
 }
 
-int main(int argc, char** argv)
-{
-	double start = omp_get_wtime();
+int main(int argc, char **argv) {
+  double start = omp_get_wtime();
   setbuf(stdout, NULL);
   if (argc < 2)
     print_usage(argv);
 
-  int* srcs;
-  int* dsts;
+  int *srcs;
+  int *dsts;
   int n;
   unsigned m;
-  int* out_array;
-  int* in_array;
-  unsigned* out_degree_list;
-  unsigned* in_degree_list;
-  int* scc_maps;
+  int *out_array;
+  int *in_array;
+  unsigned *out_degree_list;
+  unsigned *in_degree_list;
+  int *scc_maps;
   int max_deg_vert;
   double avg_degree;
   int vert_cutoff = 10000;
@@ -440,16 +417,16 @@ int main(int argc, char** argv)
 #endif
 
   double end = omp_get_wtime();
-  printf("\n\n Took %f for reading files\n\n",end-start);
+  printf("\n\n Took %f for reading files\n\n", end - start);
   start = omp_get_wtime();
 
-  create_csr(n, m, srcs, dsts, 
-    out_array, in_array,
-    out_degree_list, in_degree_list,
-    max_deg_vert, avg_degree);
-  graph g = {n, m, out_array, in_array, out_degree_list, in_degree_list, 0, NULL, NULL, NULL}; //keeping NULL at start to create the obejct
-  delete [] srcs;
-  delete [] dsts;
+  create_csr(n, m, srcs, dsts, out_array, in_array, out_degree_list,
+             in_degree_list, max_deg_vert, avg_degree);
+  graph g = {
+      n, m,    out_array, in_array, out_degree_list, in_degree_list,
+      0, NULL, NULL,      NULL}; // keeping NULL at start to create the obejct
+  delete[] srcs;
+  delete[] dsts;
 
 #if VERBOSE
   elt = timer() - elt;
@@ -462,50 +439,45 @@ int main(int argc, char** argv)
   double exec_time = timer();
 #endif
 
-  run_scc(g, scc_maps,
-    max_deg_vert, avg_degree, vert_cutoff);
+  run_scc(g, scc_maps, max_deg_vert, avg_degree, vert_cutoff);
 
 #if TIMING
   exec_time = timer() - exec_time;
   printf("Multistep SCC time: %9.6lf\n", exec_time);
 #endif
 
-//  scc_verify(g, scc_maps);
+  //  scc_verify(g, scc_maps);
 
 #if VERBOSE
   elt = timer() - elt;
   printf("Done, %9.6lf\n", elt);
 #endif
 
-
   end = omp_get_wtime();
-  printf("\n\n Scc done in %f secs \n\n",end-start);
+  printf("\n\n Scc done in %f secs \n\n", end - start);
 
-
-  
-//////////////////////////////////////////
+  //////////////////////////////////////////
   g.scc_map = scc_maps;
   update_g_with_scc(g);
-  printf("%p is address of scc_map\n",g.scc_map);
+  printf("%p is address of scc_map\n", g.scc_map);
 #if PRINTSCC
   print_scc(g);
 #endif
 
-//////////////////////////////////////////
-//////////////////////////////////////////
+  //////////////////////////////////////////
+  //////////////////////////////////////////
   start = omp_get_wtime();
   dynamic(g, g.n, g.scc_map, argv[2]);
   end = omp_get_wtime();
 
-  printf("\n\n Fully update in %f secs \n\n",end-start);
+  printf("\n\n Fully update in %f secs \n\n", end - start);
 
-//////////////////////////////////////////
-
+  //////////////////////////////////////////
 
   /*if (argc == 3)
     output_scc(g, scc_maps, argv[2]);*/
 
-#if PRINTSCC 
+#if PRINTSCC
   print_scc(g);
 #endif
 
